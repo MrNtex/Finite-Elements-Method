e7f93b3f19a0f1dd258cf374500c390d1ab1f832
MrNtex <ntexyt@gmail.com>
2025-10-25 21:05:34 +0200

    refactor: Moved jacobian generation to the constructor

--- jacobian.py
+++ jacobian.py
@@ -9,9 +9,9 @@ class UniversalJacobian:
   dN_d_epsilon: np.matrix
   dN_d_eta: np.matrix
 
-def get_universal_jacobian() -> UniversalJacobian:
-    dN_d_xi = np.zeros((NUMBER_OF_INTEGRATION_POINTS**2, 4))
-    dN_d_eta = np.zeros((NUMBER_OF_INTEGRATION_POINTS**2, 4))
+  def __init__(self):
+    self.dN_d_epsilon = np.zeros((NUMBER_OF_INTEGRATION_POINTS**2, 4))
+    self.dN_d_eta = np.zeros((NUMBER_OF_INTEGRATION_POINTS**2, 4))
 
     nodes = np.array(GAUSS_QUADRATURE[NUMBER_OF_INTEGRATION_POINTS]["nodes"])
 
@@ -20,14 +20,12 @@ def get_universal_jacobian() -> UniversalJacobian:
           idx = j * NUMBER_OF_INTEGRATION_POINTS + i
 
           print(i, j, xi, eta, idx)
-          dN_d_xi[idx, 0] = -0.25 * (1 - eta)
-          dN_d_xi[idx, 1] =  0.25 * (1 - eta)
-          dN_d_xi[idx, 2] =  0.25 * (1 + eta)
-          dN_d_xi[idx, 3] = -0.25 * (1 + eta)
+          self.dN_d_epsilon[idx, 0] = -0.25 * (1 - eta)
+          self.dN_d_epsilon[idx, 1] =  0.25 * (1 - eta)
+          self.dN_d_epsilon[idx, 2] =  0.25 * (1 + eta)
+          self.dN_d_epsilon[idx, 3] = -0.25 * (1 + eta)
 
-          dN_d_eta[idx, 0] = -0.25 * (1 - xi)
-          dN_d_eta[idx, 1] = -0.25 * (1 + xi)
-          dN_d_eta[idx, 2] =  0.25 * (1 + xi)
-          dN_d_eta[idx, 3] =  0.25 * (1 - xi)
-
-    return UniversalJacobian(dN_d_epsilon=dN_d_xi, dN_d_eta=dN_d_eta)
+          self.dN_d_eta[idx, 0] = -0.25 * (1 - xi)
+          self.dN_d_eta[idx, 1] = -0.25 * (1 + xi)
+          self.dN_d_eta[idx, 2] =  0.25 * (1 + xi)
+          self.dN_d_eta[idx, 3] =  0.25 * (1 - xi)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from gauss_integration import gauss_integrate_1d, gauss_integrate_2d
-from jacobian import get_universal_jacobian
+from jacobian import UniversalJacobian
 
 print("test")
 if __name__ == '__main__':
-    print(get_universal_jacobian())
+    uj = UniversalJacobian()
+    print(uj.dN_d_epsilon)
+    print(uj.dN_d_eta)
