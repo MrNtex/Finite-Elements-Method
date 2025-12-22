43275ba1f3c1862d2e6b9e81938f40160bb7c8ce
MrNtex <ntexyt@gmail.com>
2025-12-22 18:47:28 +0100

    feat: Update element matrix functions to include zeta derivatives and enhance matrix generation

--- 3D/element_matrices.py
+++ 3D/element_matrices.py
@@ -7,49 +7,66 @@ from gauss_integration import GAUSS_QUADRATURE
 from fem_types import GlobalData
 
 def transform_local_derivatives_to_global(
-    dN_d_epsilon: np.matrix,
+    dN_d_xi: np.matrix,
     dN_d_eta: np.matrix,
+    dN_d_zeta: np.matrix,
     jacobians: List[Jacobian],
-) -> tuple[np.matrix, np.matrix]:
-    dN_d_x = np.zeros(dN_d_epsilon.shape)
+) -> tuple[np.matrix, np.matrix, np.matrix]:
+    dN_d_x = np.zeros(dN_d_xi.shape)
     dN_d_y = np.zeros(dN_d_eta.shape)
+    dN_d_z = np.zeros(dN_d_zeta.shape)
 
-    for integration_point_index in range(NUMBER_OF_INTEGRATION_POINTS**2):
-        for i in range(dN_d_epsilon.shape[1]): # Number of rows columns is number of shape functions (4)
-            dN_d_x[integration_point_index, i] = (jacobians[integration_point_index].invJ[0, 0] * dN_d_epsilon[integration_point_index, i] +
-                                                    jacobians[integration_point_index].invJ[0, 1] * dN_d_eta[integration_point_index, i])
-            dN_d_y[integration_point_index, i] = (jacobians[integration_point_index].invJ[1, 0] * dN_d_epsilon[integration_point_index, i] +
-                                                    jacobians[integration_point_index].invJ[1, 1] * dN_d_eta[integration_point_index, i])
+    num_points = dN_d_xi.shape[0] 
+    for i in range(num_points):
+        invJ = jacobians[i].invJ
+        for n in range(8):
+            dN_d_x[i, n] = (invJ[0, 0] * dN_d_xi[i, n] +
+                            invJ[0, 1] * dN_d_eta[i, n] +
+                            invJ[0, 2] * dN_d_zeta[i, n])
+            dN_d_y[i, n] = (invJ[1, 0] * dN_d_xi[i, n] +
+                            invJ[1, 1] * dN_d_eta[i, n] +
+                            invJ[1, 2] * dN_d_zeta[i, n])
+            dN_d_z[i, n] = (invJ[2, 0] * dN_d_xi[i, n] +
+                            invJ[2, 1] * dN_d_eta[i, n] +
+                            invJ[2, 2] * dN_d_zeta[i, n])
 
-    return dN_d_x, dN_d_y
+    return dN_d_x, dN_d_y, dN_d_z
 
 def generate_H_and_C_matrix(
     dN_d_x: np.matrix,
     dN_d_y: np.matrix,
+    dN_d_z: np.matrix,
     N_functions: np.matrix,
     jacobians: List[Jacobian],
     globalData: GlobalData,
 ) -> tuple[np.matrix, np.matrix]:
-    H_matrix = np.zeros((4, 4))
-    C_matrix = np.zeros((4, 4))
+    H_matrix = np.zeros((8, 8))
+    C_matrix = np.zeros((8, 8))
 
-    for ip_index_x in range(NUMBER_OF_INTEGRATION_POINTS):
-        for ip_index_y in range(NUMBER_OF_INTEGRATION_POINTS):
-            ip_index = ip_index_y * NUMBER_OF_INTEGRATION_POINTS + ip_index_x
-            weight = GAUSS_QUADRATURE[NUMBER_OF_INTEGRATION_POINTS]["weights"][ip_index_x] * GAUSS_QUADRATURE[NUMBER_OF_INTEGRATION_POINTS]["weights"][ip_index_y]
-            detJ = jacobians[ip_index].detJ
+    conductivity = globalData.Conductivity 
+    density = globalData.Density
+    specific_heat = globalData.SpecificHeat
+    
+    weights = GAUSS_QUADRATURE[NUMBER_OF_INTEGRATION_POINTS]["weights"]
 
-            partial_H_matrix = (
-                np.outer(dN_d_x[ip_index, :], dN_d_x[ip_index, :]) +
-                np.outer(dN_d_y[ip_index, :], dN_d_y[ip_index, :])
-            ) * weight * detJ * globalData.Conductivity
+    for i in range(NUMBER_OF_INTEGRATION_POINTS):       # zeta
+        for j in range(NUMBER_OF_INTEGRATION_POINTS):   # eta
+            for k in range(NUMBER_OF_INTEGRATION_POINTS): # xi
+                ip_index = i * (NUMBER_OF_INTEGRATION_POINTS**2) + j * NUMBER_OF_INTEGRATION_POINTS + k
+                weight = weights[k] * weights[j] * weights[i]
+                
+                detJ = jacobians[ip_index].detJ
+                partial_H = (
+                    np.outer(dN_d_x[ip_index, :], dN_d_x[ip_index, :]) +
+                    np.outer(dN_d_y[ip_index, :], dN_d_y[ip_index, :]) +
+                    np.outer(dN_d_z[ip_index, :], dN_d_z[ip_index, :])
+                ) * conductivity * weight * detJ
 
-            partial_C_matrix = (
-                  np.outer(N_functions[ip_index, :], N_functions[ip_index, :])
-                  * weight * detJ * globalData.Density * globalData.SpecificHeat
-            )
+                partial_C = (
+                    np.outer(N_functions[ip_index, :], N_functions[ip_index, :])
+                ) * density * specific_heat * weight * detJ
 
-            H_matrix += partial_H_matrix
-            C_matrix += partial_C_matrix
+                H_matrix += partial_H
+                C_matrix += partial_C
 
     return H_matrix, C_matrix
